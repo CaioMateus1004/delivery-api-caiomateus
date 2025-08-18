05c8c5be8550af8df82625143cc68255cc2355f1
Caio Mateus <srspine16@gmail.com>
2025-08-18 18:52:02 -0300

    second commit

--- src/main/java/com/deliverytech/delivery/dto/request/PedidoRequest.java
+++ src/main/java/com/deliverytech/delivery/dto/request/PedidoRequest.java
@@ -3,6 +3,7 @@ package com.deliverytech.delivery.dto.request;
 import java.util.List;
 
 import com.deliverytech.delivery.model.Endereco;
+import jakarta.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,8 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoRequest {
+    @NotNull
     private Long clienteId;
+    
+    @NotNull
     private Long restauranteId;
+    
+    @NotNull
     private Endereco enderecoEntrega;
+    
+    @NotNull
     private List<ItemPedidoRequest> itens;
 }
